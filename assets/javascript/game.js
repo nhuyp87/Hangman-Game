e6a70ac53300d89f7c466319b55feb6b8d0bc288
nhuyp <ypham87@gmail.com>
2017-04-02 12:43:43 -0700

    Initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// User presses any key to get started. 
+
+// A word is randomly generated and stored as a variable. 
+
+// The word length is displayed on-screen to the user.
+
+// When correctly guessed, the correct letter replaces the dash on-screen.
+
+// If incorrectly guessed, the guess is added to the bank of incorrect guesses.
+
+// And if incorrectly guessed, the number of guesses remain decreases by one.
+
+// If the user guesses the word correctly, the number of Wins increases by one. 
